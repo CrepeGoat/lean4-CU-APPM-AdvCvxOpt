4c5d8f14fa6f59195f1e33f049b341378a028119
CrepeGoat <awqatty.b@gmail.com>
2025-03-21 00:52:06 -0600

    moved common parts to variables for brevity

--- CvxOpt/_00a_metaRules.lean
+++ CvxOpt/_00a_metaRules.lean
@@ -2,16 +2,21 @@ import Init.Prelude
 
 import CvxOpt._00_IntroToOptProblems
 
+section meta_rules
+
+variable {D : Type*} {R : Type*} {f: D → R} {C: Set D}
+
+/- Rule 1: min/max transforms -/
+
 /--
 Convert a min expression to a max via the equation:
 
-min_x f(x) = -max_x (-f(x))
+$$
+\min_x f(x) = -\max_x (-f(x))
+$$
 -/
 def min_obj_to_neg_max_neg_obj
-    {D : Type*}
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [OrderedAddCommGroup R]
     {min : R}
     (hmin: MinimumOn f C min)
     : MaximumOn (fun x: D => -(f x)) C (-min)
@@ -25,13 +30,12 @@ def min_obj_to_neg_max_neg_obj
 /--
 Convert a max expression to a min via the equation:
 
-max_x f(x) = -min_x (-f(x))
+$$
+\max_x f(x) = -\min_x (-f(x))
+$$
 -/
 def max_obj_to_neg_min_neg_obj
-    {D : Type*}
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [OrderedAddCommGroup R]
     {min : R}
     (hmin: MaximumOn f C min)
     : MinimumOn (fun x: D => -(f x)) C (-min)
@@ -45,13 +49,12 @@ def max_obj_to_neg_min_neg_obj
 /--
 Convert an inf expression to a sup via the equation:
 
-inf_x f(x) = -sup_x (-f(x))
+$$
+\inf_x f(x) = -\sup_x (-f(x))
+$$
 -/
 def inf_obj_to_neg_sup_neg_obj
-    {D : Type*}
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [OrderedAddCommGroup R]
     {inf: R}
     (hinf: InfimumOn f C inf)
     : SupremumOn (fun x: D => -(f x)) C (-inf)
@@ -67,13 +70,12 @@ def inf_obj_to_neg_sup_neg_obj
 /--
 Convert a sup expression to an inf via the equation:
 
-sup_x f(x) = -inf_x (-f(x))
+$$
+\sup_x f(x) = -\inf_x (-f(x))
+$$
 -/
 def sup_obj_to_neg_inf_neg_obj
-    {D : Type*}
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [OrderedAddCommGroup R]
     {sup: R}
     (hsup: SupremumOn f C sup)
     : InfimumOn (fun x: D => -(f x)) C (-sup)
@@ -89,13 +91,13 @@ def sup_obj_to_neg_inf_neg_obj
 /--
 Convert an argmin expression to an argmax via the equation:
 
-argmin_x f(x) = argmax_x (-f(x))
+$$
+\text{argmin}_x f(x) = \text{argmax}_x (-f(x))
+$$
 -/
 def argmin_obj_to_argmax_neg_obj
-    {D : Type*} [Neg D]
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [Neg D]
+    [OrderedAddCommGroup R]
     (hargmin: ArgumentMinimumOn f C)
     : ArgumentMaximumOn (fun x: D => -(f x)) C
     := by
@@ -106,17 +108,16 @@ def argmin_obj_to_argmax_neg_obj
             case left => exact hargmin.property.left
             case right => exact min_obj_to_neg_max_neg_obj hargmin.property.right
 
-
 /--
 Convert an argmax expression to an argmin via the equation:
 
-argmax_x f(x) = argmin_x (-f(x))
+$$
+\text{argmax}_x f(x) = \text{argmin}_x (-f(x))
+$$
 -/
 def argmax_obj_to_argmin_neg_obj
-    {D : Type*} [Neg D]
-    {R : Type*} [OrderedAddCommGroup R]
-    {f: D → R}
-    {C: Set D}
+    [Neg D]
+    [OrderedAddCommGroup R]
     (hargmax: ArgumentMaximumOn f C)
     : ArgumentMinimumOn (fun x: D => -(f x)) C
     := by
@@ -126,3 +127,5 @@ def argmax_obj_to_argmin_neg_obj
             constructor
             case left => exact hargmax.property.left
             case right => exact max_obj_to_neg_min_neg_obj hargmax.property.right
+
+end meta_rules
