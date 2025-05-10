f84f6ee0a073db7d948787330897f55d98bf9dd8
CrepeGoat <awqatty.b@gmail.com>
2025-05-10 17:18:44 -0600

    wip: shortened `nhd0_contains_neg`

--- CvxOpt/_01_TypesOfMinimizers_IntroConvexity.lean
+++ CvxOpt/_01_TypesOfMinimizers_IntroConvexity.lean
@@ -152,37 +152,6 @@ theorem local_min_on_open_set_then_critical_point
     -/
     sorry
 
-private def nhd0_contains_neg
-    : ∀ nhd0 ∈ nhds (0 : ℝ), ∃ x ∈ nhd0, x < 0
-    := by
-    intro nhd0 hNhd0
-    have hIoo0 := mem_nhds_iff_exists_Ioo_subset.mp hNhd0
-    constructor
-    case w => exact hIoo0.choose * (1 / 2)
-
-    have _0inIoo := hIoo0.choose_spec.choose_spec.left
-    rw [← Set.Ioo_def] at _0inIoo
-    simp at _0inIoo
-    have hLLt0 := _0inIoo.left
-
-    constructor
-    case right => refine mul_neg_of_neg_of_pos hLLt0 one_half_pos
-
-    have hIsSubset := hIoo0.choose_spec.choose_spec.right
-    rw [(Set.Ioo hIoo0.choose hIoo0.choose_spec.choose).subset_def] at hIsSubset
-    apply hIsSubset
-    rw [← Set.Ioo_def]
-    constructor
-
-    nth_rewrite 1 [← mul_one hIoo0.choose]
-    refine mul_lt_mul_of_neg_left one_half_lt_one hLLt0
-
-    apply lt_of_le_of_lt
-    case h.left.a.right.b => exact 0
-    case h.left.a.right.hbc => exact _0inIoo.right
-    rw [le_iff_lt_or_eq]; apply Or.intro_left
-    refine mul_neg_of_neg_of_pos hLLt0 one_half_pos
-
 def SaddlePoint
     [LE R] [RCLike R] [NormedAddCommGroup D] [InnerProductSpace R D] [CompleteSpace D]
     (f: D → R)
@@ -190,6 +159,15 @@ def SaddlePoint
     (x : D)
     : Prop
     := CriticalPoint f C x ∧ ¬LocalMinimizer f C x
+
+private def nhd0_contains_neg
+    : ∀ nhd0 ∈ nhds (0 : ℝ), ∃ x ∈ nhd0, x < 0
+    := by
+    intro nhd0 hNhd0
+    rcases mem_nhds_iff_exists_Ioo_subset.mp hNhd0 with ⟨l, r, ⟨hl, hr⟩, hlr⟩
+    refine ⟨l/2, hlr ?_, by linarith⟩
+    simp only [Set.mem_Ioo]
+    exact ⟨by linarith, by linarith⟩
 
 example
     -- [PreirreducibleSpace ℝ]
